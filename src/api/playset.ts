fb22e5bc4a84e0e18d00153e451aaf9fbe886212
wArrnp <gtobio11@gmail.com>
2020-07-15 21:15:42 +0900

    Modify getPlaysets function, it can use keyword argument for filter

--- src/api/playset.ts
+++ src/api/playset.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-export const getPlaysets = async (): Promise<Playset[]> => {
-  const res = await axios.get(`${process.env.REACT_APP_API_ROOT_URI}/v1/playset`);
+export const getPlaysets = async (keyword?: string): Promise<Playset[]> => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_ROOT_URI}/v1/playset`,
+    {
+      params: {
+        keyword
+      }
+    }
+  );
 
   return res.data.result;
 };
