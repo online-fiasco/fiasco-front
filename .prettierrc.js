8c9e1df56f6040b532f681e23a41f47f3210bf60
wArrnp <gtobio11@gmail.com>
2020-07-16 22:19:08 +0900

    Set eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports =  {
+  "singleQuote": true, 
+  "semi": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "printWidth": 120,
+  "orderedImports": true,
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false
+};
